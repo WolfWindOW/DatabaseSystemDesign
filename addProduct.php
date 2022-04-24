823b3f1fc76ad48c6a2320bfcb8635270532c34f
TexasHolden <70984537+TexasHolden@users.noreply.github.com>
2022-04-24 18:10:29 -0400

    Update addProduct.php

--- addProduct.php
+++ addProduct.php
@@ -1,17 +1,20 @@
 <?php
-//missing connection to db for now
+session_start();
+$conn = mysqli_connect("localhost", "root", "", "warehouse_management");
+
 $sku_question = $_GET["sku"];
-$total_count_question = $_GET["total_count"];
-$piece_count_question = $_GET["piece_count"];
-$pallet_type_question = $_GET["pallet_type"];
-//$customer_id_question = $_GET["customer_id"];
+$customer_question = $_GET["customer"];
+$pallet_count_question = $_GET["pallet_count"];
+$unit_count_question = $_GET["unit_count"];
+$unit_type_question = $_GET["unit_type"];
 
 //Test for inputing variables
-//echo "<h2>you inputed: $sku_question $total_count_question $piece_count_question $pallet_type_question</h2>";
+//echo "<h2>you inputed: $sku_question $customer_question $pallet_count_question $unit_count_question $unit_type_question</h2>";
 
-$sql = "INSERT INTO Product (SKU, Total_count, Piece_count, Pallet_type) VALUES ('sku', 'total_count', 'piece_count', 'pallet_type')";
-$result = $mysqli->query($sql);
+$sql = "INSERT INTO Product (SKU, Customer, Pallet_count, Unit_count, Unit_type) VALUES ('sku', 'customer','pallet_count','unit_count','unit_type')";
+$result = $mysqli_query($conn, $sql);
 
+$conn->close();
 
 ?>
 <a href="index.php">Return to main page</a>
