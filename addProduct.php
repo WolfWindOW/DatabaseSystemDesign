5e33e65dd972ea066dd107d913469ddee96c4bd5
TexasHolden <70984537+TexasHolden@users.noreply.github.com>
2022-04-25 16:28:35 -0400

    Update addProduct.php

--- addProduct.php
+++ addProduct.php
@@ -11,8 +11,8 @@ $unit_type_question = $_GET["unit_type"];
 //Test for inputing variables
 //echo "<h2>you inputed: $sku_question $customer_question $pallet_count_question $unit_count_question $unit_type_question</h2>";
 
-$sql = "INSERT INTO Product (SKU, Customer, Pallet_count, Unit_count, Unit_type) VALUES ('sku', 'customer','pallet_count','unit_count','unit_type')";
-$result = $mysqli_query($conn, $sql);
+$sql = "INSERT INTO Product (SKU, Customer, Pallet_count, Unit_count, Unit_type) VALUES ('$sku_question', '$customer_question',$pallet_count_question,$unit_count_question,'$unit_type_question')";
+$result = mysqli_query($conn, $sql);
 
 $conn->close();
 
